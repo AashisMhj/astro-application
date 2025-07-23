6e36573d68f374b3477f69cc28ec00e0e96f845b
Aashismhj <aashismhj5511@gmail.com>
2025-07-23 15:49:05 +0545

    changed allowed host config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,15 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	vite: {
-    preview: {
-      host: true,
-      port: 3000,
-      allowedHosts: ['ec2-54-161-15-209.compute-1.amazonaws.com','*']
-    }
-  }
+  site: "https://example.com",
+  integrations: [mdx(), sitemap()],
+  vite: {
+    server: {
+      allowedHosts: ["ec2-54-161-15-209.compute-1.amazonaws.com", "*"],
+    },
+  },
 });
